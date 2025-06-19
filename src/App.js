eda9d3126650537bc943b9627b014d8f98e5727d
kalaiselvipalani02 <kalaiselvi.palani02@gmail.com>
2025-06-19 15:02:58 +0530

    applying inline css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+import "./UserProfile.css";
+import style from "./UserProfile.module.css";
 //Traditional === html page., but in react  Component
 const App = () => {
-  //Basic syntax
-  const element = <h2>Hello World!</h2>;
-  //return element;
-  //Embedded expressions
-  const name = "ReactJs Developer";
-  const element2 = (
-    <h1>
-      Hello, {name} {2 + 3}
-    </h1>
-  );
-  // return element2;
-
-  //JSX as an expression
-  const element3 = (
-    <div>
-      <h1>Hello, World!</h1>
-      <h2>It is {new Date().toString()}</h2>
+  //Inline style object for the conatiner
+  const containerStyle = {
+    textAlign: "center",
+    margin: "20px",
+  };
+  return (
+    <div style={containerStyle}>
+      <img
+        src="https://media.istockphoto.com/id/2181735944/photo/natural-mountains-landscapes.jpg?s=1024x1024&w=is&k=20&c=OXwSsyYxIaOa3Y_Q2kGKNssH1frc0FLoRuQl4Xj3fnU="
+        alt="Nature"
+        //apply style for image inline style
+        style={{
+          width: "150px",
+          height: "150px",
+          borderRadius: "50%",
+          objectFit: "cover",
+        }}
+      />
+      <h2 className="name">Kalaiselvi Palanisamy</h2>
+      <p className="bio">Full Stack Developer</p>
+      <ul className={style.hobbies}>
+        <li className={style.hobby}>Coding</li>
+        <li className={style.hobby}>Singing</li>
+      </ul>
     </div>
   );
-  return element3;
 };
 
 export default App;
