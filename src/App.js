5be26eda4a2e0b7f65185d3ceb7bee3ecefc7375
kalaiselvipalani02 <kalaiselvi.palani02@gmail.com>
2025-06-19 15:24:58 +0530

    working with Bootstrap

--- src/App.js
+++ src/App.js
@@ -8,20 +8,30 @@ const App = () => {
     margin: "20px",
   };
   return (
-    <div class="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
-      <img
-        class="mx-auto block h-24 rounded-full sm:mx-0 sm:shrink-0"
-        src="/img/erin-lindford.jpg"
-        alt=""
-      />
-      <div class="space-y-2 text-center sm:text-left">
-        <div class="space-y-0.5">
-          <p class="text-lg font-semibold text-black">Erin Lindford</p>
-          <p class="font-medium text-gray-500">Product Engineer</p>
-        </div>
-        <button class="border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 ...">
-          Message
-        </button>
+    <div>
+      <div className="alert alert-primary" role="alert">
+        A simple primary alert—check it out!
+      </div>
+      <div className="alert alert-secondary" role="alert">
+        A simple secondary alert—check it out!
+      </div>
+      <div className="alert alert-success" role="alert">
+        A simple success alert—check it out!
+      </div>
+      <div className="alert alert-danger" role="alert">
+        A simple danger alert—check it out!
+      </div>
+      <div className="alert alert-warning" role="alert">
+        A simple warning alert—check it out!
+      </div>
+      <div className="alert alert-info" role="alert">
+        A simple info alert—check it out!
+      </div>
+      <div className="alert alert-light" role="alert">
+        A simple light alert—check it out!
+      </div>
+      <div className="alert alert-dark" role="alert">
+        A simple dark alert—check it out!
       </div>
     </div>
   );
