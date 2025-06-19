26583d6a225b0fbe44faa05759b020f3e75b9f5a
kalaiselvipalani02 <kalaiselvi.palani02@gmail.com>
2025-06-19 15:17:22 +0530

    tailwind css

--- src/App.js
+++ src/App.js
@@ -8,24 +8,21 @@ const App = () => {
     margin: "20px",
   };
   return (
-    <div style={containerStyle}>
+    <div class="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
       <img
-        src="https://media.istockphoto.com/id/2181735944/photo/natural-mountains-landscapes.jpg?s=1024x1024&w=is&k=20&c=OXwSsyYxIaOa3Y_Q2kGKNssH1frc0FLoRuQl4Xj3fnU="
-        alt="Nature"
-        //apply style for image inline style
-        style={{
-          width: "150px",
-          height: "150px",
-          borderRadius: "50%",
-          objectFit: "cover",
-        }}
+        class="mx-auto block h-24 rounded-full sm:mx-0 sm:shrink-0"
+        src="/img/erin-lindford.jpg"
+        alt=""
       />
-      <h2 className="name">Kalaiselvi Palanisamy</h2>
-      <p className="bio">Full Stack Developer</p>
-      <ul className={style.hobbies}>
-        <li className={style.hobby}>Coding</li>
-        <li className={style.hobby}>Singing</li>
-      </ul>
+      <div class="space-y-2 text-center sm:text-left">
+        <div class="space-y-0.5">
+          <p class="text-lg font-semibold text-black">Erin Lindford</p>
+          <p class="font-medium text-gray-500">Product Engineer</p>
+        </div>
+        <button class="border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 ...">
+          Message
+        </button>
+      </div>
     </div>
   );
 };
