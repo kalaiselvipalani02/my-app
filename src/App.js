8a8f360f0ba20799ad0f509d2521251881ee941e
kalaiselvipalani02 <kalaiselvi.palani02@gmail.com>
2025-06-19 14:28:16 +0530

    understand JSX

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 //Traditional === html page., but in react  Component
 const App = () => {
-  return <h1>This is my first React app</h1>;
+  //Basic syntax
+  const element = <h2>Hello World!</h2>;
+  //return element;
+  //Embedded expressions
+  const name = "ReactJs Developer";
+  const element2 = (
+    <h1>
+      Hello, {name} {2 + 3}
+    </h1>
+  );
+  // return element2;
+
+  //JSX as an expression
+  const element3 = (
+    <div>
+      <h1>Hello, World!</h1>
+      <h2>It is {new Date().toString()}</h2>
+    </div>
+  );
+  return element3;
 };
 
 export default App;
