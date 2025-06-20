873b8a5324305339bd0be876bcacaa325a408e16
kalaiselvipalani02 <kalaiselvi.palani02@gmail.com>
2025-06-20 11:22:50 +0530

    implement useReducer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import { createContext } from "react";
 import "./App.css";
-import CounterContext, { CounterProvider } from "./components/CounterContext ";
+
 import CounterLogic from "./components/CounterLogic";
 
 const App = () => {
   return (
-    <CounterProvider>
-      <div className="app-container">
-        <h1 className="title">React Counter</h1>
-        <p className="sub-description">
-          A simple counter application using Context API
-        </p>
+    <div className="app-container">
+      <h1 className="title">React Counter</h1>
+      <p className="sub-description">
+        A simple counter application using Context API
+      </p>
 
-        <CounterLogic />
-      </div>
-    </CounterProvider>
+      <CounterLogic />
+    </div>
   );
 };
 
--- src/components/CounterLogic.js
+++ src/components/CounterLogic.js
@@ -1,18 +1,37 @@
-import React, { useContext } from "react";
-import CounterContext from "./CounterContext ";
+import React, { useReducer } from "react";
+
+const initialState = {
+  count: 0,
+};
+
+const reducer = (state, action) => {
+  if (action.type === "INCREMENT") {
+    return { count: state.count + 1 };
+  } else if (action.type === "DECREMENT") {
+    return { count: state.count - 1 };
+  } else if (action.type === "RESET") {
+    return { count: initialState.count };
+  }
+};
 const CounterLogic = () => {
-  const { count, increment, decrement, reset } = useContext(CounterContext);
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div>
       <div className="counter-container">
-        <h1> {count}</h1>
-        <button className="button" onClick={increment}>
+        <h1>{state.count} </h1>
+        <button
+          className="button"
+          onClick={() => dispatch({ type: "INCREMENT" })}
+        >
           +
         </button>
-        <button className="button" onClick={decrement}>
+        <button
+          className="button"
+          onClick={() => dispatch({ type: "DECREMENT" })}
+        >
           -
         </button>
-        <button className="button" onClick={reset}>
+        <button className="button" onClick={() => dispatch({ type: "RESET" })}>
           Reset
         </button>
       </div>
